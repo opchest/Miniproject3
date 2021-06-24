79d16b24bf23f50c259c0a1a9ee414a348b78213
opchest <jay231941@gmail.com>
2021-06-24 18:48:48 +0800

    myAI.cpp

--- myAI.cpp
+++ myAI.cpp
@@ -7,3 +7,37 @@
 
 using namespace std;
 
+struct Point {
+    int x, y;
+};
+
+int player;
+const int SIZE = 8;
+array<array<int, SIZE>, SIZE> board;
+vector<Point> next_valid_points;
+
+void read_board(ifstream& fin) {
+    fin >> player;
+    for(int i = 0; i < SIZE; i++) {
+        for(int j = 0; j < SIZE; j++) 
+            fin >> board[i][j];
+    }
+}
+
+void read_valid_points(ifstream& fin) {
+    int n_valid_points;
+    int x, y;
+    fin >> n_valid_points;
+    for(int i = 0; i < n_valid_points; i++) {
+        fin >> x >> y;
+        next_valid_points.push_back({x, y});
+    }
+}
+
+void write_valid_point(ofstream& fout) {
+    int n_valid_points = next_valid_points.size();
+    int idx;
+    Point p = next_valid_points[idx];
+    fout << p.x << " " << p.y << endl;
+    fout.flush();
+}
